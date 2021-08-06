0680d709bef2a0339eb95f8e65babbb4ed0bc329
danOSU <liyanage.5@osu.edu>
2021-08-06 14:51:27 -0500

    calculated observables for 150 design points that Ozge ran

--- vah_design/collect_all_observables_to_df.py
+++ vah_design/collect_all_observables_to_df.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+from pathlib import Path
 # 8 bins
 ALICE_cent_bins = np.array([[0,5],[5,10],[10,20],[20,30],[30,40],[40,50],[50,60],[60,70]])
 
@@ -29,15 +29,24 @@ obs_cent_list = {
     }
 }
 
+float_t = '<f8'
+number_of_models_per_run = 4
+
+sdtype = [('Pb-Pb-2760',
+                         [(obs, [("mean", float_t, len(cent_list)),
+                                                       ("err", float_t, len(cent_list))]) \
+                                          for obs, cent_list in obs_cent_list['Pb-Pb-2760'].items()],
+                      number_of_models_per_run)]
 
 df_mean = {}
 df_sd = {}
 for i in range(0,150):
-    result_data = np.fromfile(f"~/vah_run_events/ozge_events/{i}/obs_Pb-Pb-2760.dat", dtype = sdtype)
+    file_path = Path('/home/ac.liyanage/vah_run_events/ozge_events/'+str(i)+'/obs_Pb-Pb-2760.dat')
+    result_data = np.fromfile(file_path, dtype = sdtype)
     for obs, cent_list in obs_cent_list['Pb-Pb-2760'].items():
         for i,cen in enumerate(cent_list):
-            mean=MAP_data1["Pb-Pb-2760"][obs]["mean"][0][0][i]
-            sd=MAP_data1["Pb-Pb-2760"][obs]["err"][0][0][i]
+            mean=result_data["Pb-Pb-2760"][obs]["mean"][0][0][i]
+            sd=result_data["Pb-Pb-2760"][obs]["err"][0][0][i]
 
 
         obs_name=obs+'_'+str(cen)
@@ -47,8 +56,8 @@ for i in range(0,150):
             df_mean[obs_name].append(mean)
             df_sd[obs_name].append(sd)
         else:
-            df_mean[obs_name] = mean
-            df_sd[obs_name] = sd
+            df_mean[obs_name] = [mean]
+            df_sd[obs_name] = [sd]
 
 df1 = pd.DataFrame(data=df_mean, index=np.arange(150))
 df2 = pd.DataFrame(data=df_sd, index=np.arange(150))
