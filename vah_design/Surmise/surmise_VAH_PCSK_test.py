30899974e94f2d14c79f958061427592fe1e91eb
ozgesurer <ozgesurer2019@u.northwestern.edu>
2022-02-16 11:49:31 -0600

    SK

--- vah_design/Surmise/surmise_VAH_PCSK_test.py
+++ vah_design/Surmise/surmise_VAH_PCSK_test.py
@@ -15,27 +15,27 @@ design = design.drop(labels='tau_initial', axis=1)
 colnames = design.columns
 theta = design.head(300)
 drop_index = np.array([19, 23, 31, 32, 71, 91, 92, 98, 129, 131, 146, 162, 171, 174, 184, 190, 194, 195, 198])
-theta200 = theta.drop(index=drop_index)
-df_mean200 = df_mean.drop(index=drop_index)
-df_sd200 = df_sd.drop(index=drop_index)
+theta = theta.drop(index=drop_index)
+df_mean = df_mean.drop(index=drop_index)
+df_sd = df_sd.drop(index=drop_index)
 
 # A batch of 90 points with 800 events 
 df_mean_b0 = pd.read_csv('../simulation_data/mean_for_90_add_batch0_800_events_design', index_col=0)
 df_sd_b0 = pd.read_csv('../simulation_data/sd_for_90_add_batch0_800_events_design', index_col=0)
 design_b0 = pd.read_csv('../design_data/add_design_122421.txt', delimiter = ' ')
 drop_index_b0 = np.array([10, 17, 27, 35, 49, 58])
-design800_b0 = design_b0.drop(index=drop_index_b0)
-df_mean800_b0 = df_mean_b0.drop(index=drop_index_b0)
-df_sd800_b0 = df_sd_b0.drop(index=drop_index_b0)
+design_b0 = design_b0.drop(index=drop_index_b0)
+df_mean_b0 = df_mean_b0.drop(index=drop_index_b0)
+df_sd_b0 = df_sd_b0.drop(index=drop_index_b0)
 
 # A batch of 90 points with 800 events 
 df_mean_b1 = pd.read_csv('../simulation_data/mean_for_90_add_batch1_800_events_design', index_col=0)
 df_sd_b1 = pd.read_csv('../simulation_data/sd_for_90_add_batch1_800_events_design', index_col=0)
 design_b1 = pd.read_csv('../design_data/add_design_122721.txt', delimiter = ' ')
 drop_index_b1 = np.array([0, 3, 6, 16, 18, 20, 26, 33, 37, 41, 48])
-design800_b1 = design_b1.drop(index=drop_index_b1)
-df_mean800_b1 = df_mean_b1.drop(index=drop_index_b1)
-df_sd800_b1 = df_sd_b1.drop(index=drop_index_b1)
+design_b1 = design_b1.drop(index=drop_index_b1)
+df_mean_b1 = df_mean_b1.drop(index=drop_index_b1)
+df_sd_b1 = df_sd_b1.drop(index=drop_index_b1)
 
 # A batch of 90 points with 800 events 
 df_mean_b2 = pd.read_csv("../simulation_data/mean_for_90_sliced_test_design_800_events_design", index_col=0)
@@ -44,9 +44,14 @@ design_b2 = pd.read_csv('../design_data/sliced_VAH_090321_test.txt', delimiter =
 design_b2 = design_b2.drop(labels='tau_initial', axis=1)
 design_b2 = design_b2.iloc[0:90]
 drop_index_vl = np.array([29, 35, 76, 84, 87])
-design800_b2 = design_b2.drop(index=drop_index_vl)
-df_mean800_b2 = df_mean_b2.drop(index=drop_index_vl)
-df_sd800_b2 = df_sd_b2.drop(index=drop_index_vl)
+design_b2 = design_b2.drop(index=drop_index_vl)
+df_mean_b2 = df_mean_b2.drop(index=drop_index_vl)
+df_sd_b2 = df_sd_b2.drop(index=drop_index_vl)
+
+# A batch of 75 points with 1600 events 
+df_mean_b3 = pd.read_csv("../simulation_data/mean_for_75_batch0_design_1600_events_design", index_col=0)
+df_sd_b3 = pd.read_csv("../simulation_data/sd_for_75_batch0_design_1600_events_design", index_col=0)
+design_b3 = pd.read_csv('../design_data/add_design_020522.txt', delimiter = ' ')
 
 # Read the experimental data
 exp_data = pd.read_csv('../PbPb2760_experiment', index_col=0)
@@ -70,10 +75,11 @@ for i in exp_data.columns:
     x.append(words[0])
 
 # Only keep simulation data that we have corresponding experimental data
-df_mean200 = df_mean200[exp_label]
-df_mean800_b0 = df_mean800_b0[exp_label]
-df_mean800_b1 = df_mean800_b1[exp_label]
-df_mean800_b2 = df_mean800_b2[exp_label]
+df_mean = df_mean[exp_label]
+df_mean_b0 = df_mean_b0[exp_label]
+df_mean_b1 = df_mean_b1[exp_label]
+df_mean_b2 = df_mean_b2[exp_label]
+df_mean_b3 = df_mean_b3[exp_label]
 
 selected_observables = exp_label[0:-32]
 
@@ -84,36 +90,39 @@ y_sd = y_sd[0:-32]
 
 #print(f'Last item on the selected observable is {selected_observables[-1]}')
 
-df_mean200 = df_mean200[selected_observables]
-df_mean800_b0 = df_mean800_b0[selected_observables]
-df_mean800_b1 = df_mean800_b1[selected_observables]
-df_mean800_b2 = df_mean800_b2[selected_observables]
+df_mean = df_mean[selected_observables]
+df_mean_b0 = df_mean_b0[selected_observables]
+df_mean_b1 = df_mean_b1[selected_observables]
+df_mean_b2 = df_mean_b2[selected_observables]
+df_mean_b3 = df_mean_b3[selected_observables]
 
-df_sd200 = df_sd200[selected_observables]
-df_sd800_b0 = df_sd800_b0[selected_observables]
-df_sd800_b1 = df_sd800_b1[selected_observables]
-df_sd800_b2 = df_sd800_b2[selected_observables]
+df_sd = df_sd[selected_observables]
+df_sd_b0 = df_sd_b0[selected_observables]
+df_sd_b1 = df_sd_b1[selected_observables]
+df_sd_b2 = df_sd_b2[selected_observables]
+df_sd_b3 = df_sd_b3[selected_observables]
 
-print('Total obs.:', df_mean200.shape[0] + df_mean800_b0.shape[0] + df_mean800_b1.shape[0] + df_mean800_b2.shape[0])
+print('Total obs.:', df_mean.shape[0] + df_mean_b0.shape[0] + df_mean_b1.shape[0] + df_mean_b2.shape[0] + df_mean_b3.shape[0])
 
 
-feval800 = pd.concat([df_mean800_b0, df_mean800_b1, df_mean800_b2])
-sd800 = pd.concat([df_sd800_b0, df_sd800_b1, df_sd800_b2])
-design800 = pd.concat([design800_b0, design800_b1, design800_b2])
+feval_add = pd.concat([df_mean_b0, df_mean_b1, df_mean_b2, df_mean_b3])
+sd_add = pd.concat([df_sd_b0, df_sd_b1, df_sd_b2, df_sd_b3])
+design_add = pd.concat([design_b0, design_b1, design_b2, design_b3])
 
 # Split test using 800
-msk = ((np.random.rand(len(feval800)) < 0.6)  +
-       (np.arange(len(feval800)) > (df_mean800_b0.shape[0] + df_mean800_b1.shape[0])))
-df_mean_train800 = feval800[msk]
-df_sd_train800 = sd800[msk]
-design_train800 = design800[msk]
+msk = ((np.random.rand(len(feval_add)) < 0.6)  +
+       (np.arange(len(feval_add)) > (df_mean_b0.shape[0] + df_mean_b1.shape[0])))
 
-df_mean_test = feval800[~msk]
-theta_validation = design800[~msk]
+feval_add_train = feval_add[msk]
+sd_add_train = sd_add[msk]
+design_add_train = design_add[msk]
 
-feval = pd.concat([df_mean200, df_mean_train800])
-sdeval = pd.concat([df_sd200, df_sd_train800])
-theta = pd.concat([theta200, design_train800])
+df_mean_test = feval_add[~msk]
+theta_validation = design_add[~msk]
+
+feval = pd.concat([df_mean, feval_add_train])
+sdeval = pd.concat([df_sd, sd_add_train])
+theta = pd.concat([theta, design_add_train])
 
 # Final training/test data
 theta = theta.to_numpy()
@@ -122,26 +131,12 @@ sdeval = sdeval.to_numpy()
 theta_test = theta_validation.to_numpy()
 feval_test = df_mean_test.to_numpy()
 
-assert feval.shape[0] + feval_test.shape[0] == df_mean200.shape[0] + df_mean800_b0.shape[0] + df_mean800_b1.shape[0] + df_mean800_b2.shape[0]
+#assert feval.shape[0] + feval_test.shape[0] == df_mean200.shape[0] + df_mean800_b0.shape[0] + df_mean800_b1.shape[0] + df_mean800_b2.shape[0]
 
 feval = np.transpose(feval)
 sdeval = np.transpose(sdeval)
 feval_test = np.transpose(feval_test)
 
-
-# Build an emulator
-emu_tr = emulator(x=x_np,
-                   theta=theta,
-                   f=feval,
-                   method='PCGPwM',
-                   args={'epsilonPC':  0.1})
-pred_test = emu_tr.predict(x=x_np, theta=theta_test)
-pred_test_mean = pred_test.mean()
-pred_test_var = pred_test.var()
-
-sse = np.sum((pred_test_mean - feval_test) ** 2,1)
-sst = np.sum((feval_test.T - np.mean(feval_test,1)) ** 2,0)
-rsqwM = 1-sse/sst
 
 
 # Build another emulator
@@ -156,6 +151,12 @@ pred_test_mean = pred_test.mean()
 pred_test_var = pred_test.var()
 
 
+# Check error
+errors_test = (pred_test_mean - feval_test).flatten()
+sst = np.sum((feval_test.flatten() - np.mean(feval_test.flatten()))**2)
+print('MSE test=', np.mean(errors_test**2))
+print('rsq test=', 1 - np.sum(errors_test**2)/sst)
+
 rsq = []
 for i in range(pred_test_mean.shape[0]):
     sse = np.sum((pred_test_mean[i, :] - feval_test[i, :])**2)
@@ -168,32 +169,10 @@ plt.xlabel('observables')
 plt.ylabel('test rsq')
 plt.show()
 
-
-sse = np.sum((pred_test_mean - feval_test) ** 2,1)
-sst = np.sum((feval_test.T - np.mean(feval_test,1)) ** 2,0)
-rsqsk = 1-sse/sst
-
-plt.plot(rsqwM, rsqwM, 'k-', lw=2)
-plt.plot(rsqsk, rsqsk, 'k-', lw=2)
-plt.scatter(rsqwM,rsqsk)
-plt.xlabel('PCGPwM rsq')
-plt.ylabel('PCSK rsq')
+# Observe test prediction
+fig = plt.figure()
+plt.scatter(feval_test, pred_test_mean, alpha=0.5)
+plt.plot(range(0, 5), range(0, 5), color='red')
+plt.xlabel('Simulator outcome (test)')
+plt.ylabel('Emulator prediction (test)')
 plt.show()
-
-# say we are choosing these points.
-
-# events = 2400 guess on r2
-fv = pred_test.mean().var(1)
-fpv = pred_test.var().mean(1)
-vbar = np.square(sdeval).mean(1)
-
-#
-r2guess = fv/(vbar+fv+fpv)
-plt.scatter(rsqsk,r2guess)
-plt.plot(r2guess, r2guess, 'k-', lw=2)
-plt.plot(rsqsk, rsqsk, 'k-', lw=2)
-plt.xlabel('PCSK rsq')
-plt.ylabel('predicted rsq')
-plt.show()
-#this shows this is a decent approximation, but we are overestimating the error.
-
